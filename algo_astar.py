36c6f670781f9a830dc6372001aecd43906f4a1d
Stephenxuu <145874517+Stephenxuu@users.noreply.github.com>
2025-02-24 10:39:55 -0500

    Update algo_astar.py

--- algo_astar.py
+++ algo_astar.py
@@ -1,3 +1,12 @@
+# This file implements the algorithm for fitting an always feasible metalog 2.0 distribution to (quantile, probability) data.
+# It implements Algorithm 1 from the forthcoming paper:
+#
+# "On the Properties of the Metalog Distribution" by Manel Baucells, Lonnie Chrisman, and Thomas W. Keelin
+#
+# This code is written by Stephen Xu. This code is provided freely for any purpose, including academic, commercial, and personal
+# use. There are no restrictions on modification, redistribution, or incorporation into other projects. If you find this useful,
+# we kindly request that you cite the above paper.
+
 from feasibility_stats import feasible as fb
 from feasibility_stats import compute_b_matrix as compute_b_matrix
 from feasibility_stats import M as M
